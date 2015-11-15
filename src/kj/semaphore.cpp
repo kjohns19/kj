149b847f7e1396708bc19ed7a353fd0b547d6b71
Kevin Johnson <krjohnson111@gmail.com>
2015-11-14 19:50:59 -0500

    Added Semaphore

---
+++ src/kj/semaphore.cpp
@@ -0,0 +1,34 @@
+#include <kj/semaphore.hpp>
+
+namespace kj {
+
+Semaphore::Semaphore()
+: d_count(0) {}
+Semaphore::Semaphore(int count)
+: d_count(count) {}
+
+bool Semaphore::trywait()
+{
+    std::lock_guard<std::mutex> guard(d_mutex);
+    if (d_count > 0)
+    {
+        d_count--;
+        return true;
+    }
+    return false;
+}
+void Semaphore::wait()
+{
+    std::unique_lock<std::mutex> guard(d_mutex);
+    d_cond.wait(guard, [this]() { return d_count > 0; });
+    d_count--;
+}
+void Semaphore::post()
+{
+    std::lock_guard<std::mutex> guard(d_mutex);
+    d_count++;
+    d_cond.notify_one();
+}
+
+} // close namespace kj
+
---
+++ src/kj/semaphore.hpp
@@ -0,0 +1,42 @@
+#ifndef INCLUDED_KJ_SEMAPHORE
+#define INCLUDED_KJ_SEMAPHORE
+
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+
+namespace kj {
+
+class Semaphore
+{
+public:
+    Semaphore();
+    Semaphore(int count);
+
+    bool trywait();
+    void wait();
+    template<typename Rep, typename Period>
+    bool wait(const std::chrono::duration<Rep, Period>& rel_time);
+
+    void post();
+
+    int count() const { return d_count; }
+private:
+    std::mutex d_mutex;
+    std::condition_variable d_cond;
+    int d_count;
+};
+
+template<typename Rep, typename Period>
+bool Semaphore::wait(const std::chrono::duration<Rep, Period>& rel_time)
+{
+    std::unique_lock<std::mutex> guard(d_mutex);
+    bool status = d_cond.wait_for(guard, rel_time, [this]() { return d_count > 0; });
+    if (status)
+        d_count--;
+    return status;
+}
+
+} // close namespace kj
+
+#endif //INCLUDED_KJ_SEMAPHORE
