ae34fa2b9fcff97335c835bcf6c79f2366b6b177
Kevin Johnson <krjohnson111@gmail.com>
2015-11-14 19:50:38 -0500

    Added ObjectBuffer

---
+++ src/kj/object_buffer.cpp
@@ -0,0 +1,8 @@
+#include <kj/object_buffer.hpp>
+
+namespace kj {
+
+// TODO
+
+} // close namespace kj
+
---
+++ src/kj/object_buffer.hpp
@@ -0,0 +1,20 @@
+#ifndef INCLUDED_KJ_OBJECT_BUFFER
+#define INCLUDED_KJ_OBJECT_BUFFER
+
+namespace kj {
+
+template<typename T>
+class ObjectBuffer
+{
+public:
+    T& object() { return *reinterpret_cast<T*>(&d_buffer); }
+    const T& object() const { return *reinterpret_cast<T*>(&d_buffer); }
+    void* buffer() { return d_buffer; }
+    const void* buffer() const { return d_buffer; }
+private:
+    alignas(T) char d_buffer[sizeof(T)];
+};
+
+} // close namespace kj
+
+#endif //INCLUDED_KJ_OBJECT_BUFFER
