e910f351e93219b14c0e4496178dabe5076f804e
Kevin Johnson <krjohnson111@gmail.com>
2016-03-13 19:35:26 -0400

    Added gtkmm_no_warning.hpp (wrapper around gtkmm.h to remove deprecated warning for auto_ptr)

---
+++ src/kj/gtkmm_no_warning.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDED_KJ_GTKMM_NO_WARNING_HPP
+#define INCLUDED_KJ_GTKMM_NO_WARNING_HPP
+
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <gtkmm.h>
+#pragma GCC diagnostic warning "-Wdeprecated-declarations"
+
+#endif //INCLUDED_KJ_GTKMM_NO_WARNING_HPP
